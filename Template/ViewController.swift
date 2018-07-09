e138703176f48e5e903d655a0f0cdce8b810db69
David Yu <equalbean@gmail.com>
2018-07-09 12:38:20 -0400

    init

---
+++ Template/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Template
+//
+//  Created by David Yu on 2018/7/9.
+//  Copyright © 2018年 David Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
